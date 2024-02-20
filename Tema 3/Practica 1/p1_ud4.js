abb25b1fd35ad22c0eb7affb849dc2fccf53f43e
Daniel Armada González <a23danielag@ATEIS.LOCAL>
2024-02-20 22:02:56 +0100

    Objetos Js

---
+++ Tema 3/Practica 1/p1_ud4.js
@@ -0,0 +1,34 @@
+class Vehiculo {
+
+    constructor(modelo, marca, precio, km) {
+        this.modelo = modelo;
+        this.marca = marca;
+        this.precio = precio;
+        this.km = km;
+    }
+
+
+}
+
+
+
+function iniciar() {
+
+    document.getElementById("enviar").onclick = recibirDatos;
+    console.log("iniciar");
+}
+const recibirDatos = (evt) => {
+    evt.preventDefault();
+    console.log("recibeDatos");
+
+    let modelo = document.getElementById("modelo").value;
+    let marca = document.getElementById("marca").value;
+    let precio = document.getElementById("precio").value;
+    let km = document.getElementById("km").value;
+
+  let vehiculo = new Vehiculo( modelo, marca,precio, km);
+
+    console.log(vehiculo);
+}
+
+window.onload = iniciar;
